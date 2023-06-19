f31031bada82cafd321dc2b57deb74ef05a50599
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2023-06-19 13:55:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+# trying to trigger https://next.sonarqube.com/sonarqube/coding_rules?open=pythonsecurity:S5334&rule_key=pythonsecurity:S5334
+
+from flask import request
+
+@app.route("/")
+def example():
+    operation = request.args.get("operation")
+    eval(f"product_{operation}()") # Noncompliant
+    return "OK"
+
+
 test_var = 5
 print(test_var)
-
