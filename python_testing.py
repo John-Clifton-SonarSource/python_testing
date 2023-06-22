1c9d549082044968f12c7c5e079f9be8acc215fb
John-Clifton-SonarSource <119871437+John-Clifton-SonarSource@users.noreply.github.com>
2023-06-22 12:01:11 +0100

    Update python_testing.py

--- python_testing.py
+++ python_testing.py
@@ -7,24 +7,43 @@ app = Flask(__name__)
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-counter = 10
-counter =+ 8
+def update_and_show_counter(counter):
+    counter =+ 8
+    print(counter)
 
-print(counter)
+counter = 10
+update_and_show_counter(counter)
+
+def complicated_code(text):
+    a=1
+    b=2
+    c=3
+    d=4
+
+    if a in (a,b,c,d):
+        return(c+input)
+        if a < b:
+            return(b+input)
+            if c < d:
+                return(d+input)
+                if a < c:
+                    return(a+input)
+                    if a < d:
+                        return(d)
+                        if c < d:
+                            return(a+input)
+                            if a > b:
+                                return(c)
+
+    return 0
 
 @app.route('/xss2')
 def index2():
-    return make_response(request.args.get("input"))
+    
+    return make_response(complicated_code(request.args.get("input")))
 
-def redirect_again():
-    url = request.GET.get("url", "/testing_redirect")
-    return HttpResponseRedirect(url)
-
-def counter():
-
-    current_total = 0
-
-    if current_total + 11 <= 21:
-        return 11
-    else:
-        return 1
+@app.route("/")
+def example():
+    operation = request.args.get("operation")
+    eval(f"product_{operation}()") # Noncompliant
+    return "OK"
